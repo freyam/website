42dccf4348269ede0f6d449ac735d5e68bfc4223
Freyam Mehta <frey77mehta@gmail.com>
2021-05-23 12:45:58 +0400

    Dependencies

---
+++ .nuxt/dist/server/pages/index.js
@@ -0,0 +1,111 @@
+exports.ids = [1];
+exports.modules = {
+
+/***/ 56:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=template&id=2b3c1f4e&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',[_c('Header'),_vm._ssrNode(" "),_c('about'),_vm._ssrNode(" "),_c('skill'),_vm._ssrNode(" "),_c('experience'),_vm._ssrNode(" "),_c('modal'),_vm._ssrNode(" "),_c('project'),_vm._ssrNode(" "),_c('contact')],2)}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=template&id=2b3c1f4e&
+
+// EXTERNAL MODULE: external "vuex"
+var external_vuex_ = __webpack_require__(3);
+
+// EXTERNAL MODULE: ./components/Sections/Header.vue + 4 modules
+var Header = __webpack_require__(34);
+
+// EXTERNAL MODULE: ./components/Sections/About.vue + 4 modules
+var About = __webpack_require__(32);
+
+// EXTERNAL MODULE: ./components/Sections/Skill.vue + 4 modules
+var Skill = __webpack_require__(36);
+
+// EXTERNAL MODULE: ./components/Sections/Experience.vue + 4 modules
+var Experience = __webpack_require__(33);
+
+// EXTERNAL MODULE: ./components/Widgets/Modal.vue + 4 modules
+var Modal = __webpack_require__(37);
+
+// EXTERNAL MODULE: ./components/Sections/Project.vue + 4 modules
+var Project = __webpack_require__(35);
+
+// EXTERNAL MODULE: ./components/Sections/Contact.vue + 3 modules
+var Contact = __webpack_require__(38);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/@nuxt/components/dist/loader.js??ref--0-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+
+
+
+
+
+
+
+/* harmony default export */ var lib_vue_loader_options_pagesvue_type_script_lang_js_ = ({
+  components: {
+    Header: Header["default"],
+    About: About["default"],
+    Skill: Skill["default"],
+    Experience: Experience["default"],
+    Modal: Modal["default"],
+    Project: Project["default"],
+    Contact: Contact["default"]
+  },
+
+  mounted() {
+    this.closeMenu();
+  },
+
+  methods: Object(external_vuex_["mapActions"])({
+    closeMenu: 'closeMenu'
+  })
+});
+// CONCATENATED MODULE: ./pages/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pagesvue_type_script_lang_js_ = (lib_vue_loader_options_pagesvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(0);
+
+// CONCATENATED MODULE: ./pages/index.vue
+
+
+
+
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pagesvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  null,
+  null,
+  "1b017984"
+  
+)
+
+/* harmony default export */ var pages = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=index.js.map
