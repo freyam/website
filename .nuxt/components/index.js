e08c06553f3ae8123a7bdc436ec840a6315dff4f
Freyam Mehta <frey77mehta@gmail.com>
2021-05-23 04:46:30 +0400

    Fixed Dependencied

---
+++ .nuxt/components/index.js
@@ -0,0 +1,86 @@
+export { default as ControlsMenuToggle } from '../../components/Controls/MenuToggle.vue'
+export { default as ControlsThemeToggle } from '../../components/Controls/ThemeToggle.vue'
+export { default as IconsDark } from '../../components/Icons/Dark.vue'
+export { default as IconsEnvelope } from '../../components/Icons/Envelope.vue'
+export { default as IconsExternalLink } from '../../components/Icons/ExternalLink.vue'
+export { default as IconsFacebook } from '../../components/Icons/Facebook.vue'
+export { default as IconsGitHub } from '../../components/Icons/GitHub.vue'
+export { default as IconsLight } from '../../components/Icons/Light.vue'
+export { default as IconsLinkedIn } from '../../components/Icons/LinkedIn.vue'
+export { default as IconsLogo } from '../../components/Icons/Logo.vue'
+export { default as IconsMedium } from '../../components/Icons/Medium.vue'
+export { default as IconsTimes } from '../../components/Icons/Times.vue'
+export { default as IconsTwitter } from '../../components/Icons/Twitter.vue'
+export { default as IconsYouTube } from '../../components/Icons/YouTube.vue'
+export { default as NavigationEmailHandle } from '../../components/Navigation/EmailHandle.vue'
+export { default as NavigationMobileMenu } from '../../components/Navigation/MobileMenu.vue'
+export { default as NavigationNavMenu } from '../../components/Navigation/NavMenu.vue'
+export { default as NavigationNavbar } from '../../components/Navigation/Navbar.vue'
+export { default as NavigationSocialHandles } from '../../components/Navigation/SocialHandles.vue'
+export { default as SectionsAbout } from '../../components/Sections/About.vue'
+export { default as SectionsContact } from '../../components/Sections/Contact.vue'
+export { default as SectionsExperience } from '../../components/Sections/Experience.vue'
+export { default as SectionsFooter } from '../../components/Sections/Footer.vue'
+export { default as SectionsHeader } from '../../components/Sections/Header.vue'
+export { default as SectionsProject } from '../../components/Sections/Project.vue'
+export { default as SectionsSkill } from '../../components/Sections/Skill.vue'
+export { default as WidgetsModal } from '../../components/Widgets/Modal.vue'
+
+export const LazyControlsMenuToggle = import('../../components/Controls/MenuToggle.vue' /* webpackChunkName: "components/controls-menu-toggle" */).then(c => wrapFunctional(c.default || c))
+export const LazyControlsThemeToggle = import('../../components/Controls/ThemeToggle.vue' /* webpackChunkName: "components/controls-theme-toggle" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsDark = import('../../components/Icons/Dark.vue' /* webpackChunkName: "components/icons-dark" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsEnvelope = import('../../components/Icons/Envelope.vue' /* webpackChunkName: "components/icons-envelope" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsExternalLink = import('../../components/Icons/ExternalLink.vue' /* webpackChunkName: "components/icons-external-link" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsFacebook = import('../../components/Icons/Facebook.vue' /* webpackChunkName: "components/icons-facebook" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsGitHub = import('../../components/Icons/GitHub.vue' /* webpackChunkName: "components/icons-git-hub" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsLight = import('../../components/Icons/Light.vue' /* webpackChunkName: "components/icons-light" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsLinkedIn = import('../../components/Icons/LinkedIn.vue' /* webpackChunkName: "components/icons-linked-in" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsLogo = import('../../components/Icons/Logo.vue' /* webpackChunkName: "components/icons-logo" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsMedium = import('../../components/Icons/Medium.vue' /* webpackChunkName: "components/icons-medium" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsTimes = import('../../components/Icons/Times.vue' /* webpackChunkName: "components/icons-times" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsTwitter = import('../../components/Icons/Twitter.vue' /* webpackChunkName: "components/icons-twitter" */).then(c => wrapFunctional(c.default || c))
+export const LazyIconsYouTube = import('../../components/Icons/YouTube.vue' /* webpackChunkName: "components/icons-you-tube" */).then(c => wrapFunctional(c.default || c))
+export const LazyNavigationEmailHandle = import('../../components/Navigation/EmailHandle.vue' /* webpackChunkName: "components/navigation-email-handle" */).then(c => wrapFunctional(c.default || c))
+export const LazyNavigationMobileMenu = import('../../components/Navigation/MobileMenu.vue' /* webpackChunkName: "components/navigation-mobile-menu" */).then(c => wrapFunctional(c.default || c))
+export const LazyNavigationNavMenu = import('../../components/Navigation/NavMenu.vue' /* webpackChunkName: "components/navigation-nav-menu" */).then(c => wrapFunctional(c.default || c))
+export const LazyNavigationNavbar = import('../../components/Navigation/Navbar.vue' /* webpackChunkName: "components/navigation-navbar" */).then(c => wrapFunctional(c.default || c))
+export const LazyNavigationSocialHandles = import('../../components/Navigation/SocialHandles.vue' /* webpackChunkName: "components/navigation-social-handles" */).then(c => wrapFunctional(c.default || c))
+export const LazySectionsAbout = import('../../components/Sections/About.vue' /* webpackChunkName: "components/sections-about" */).then(c => wrapFunctional(c.default || c))
+export const LazySectionsContact = import('../../components/Sections/Contact.vue' /* webpackChunkName: "components/sections-contact" */).then(c => wrapFunctional(c.default || c))
+export const LazySectionsExperience = import('../../components/Sections/Experience.vue' /* webpackChunkName: "components/sections-experience" */).then(c => wrapFunctional(c.default || c))
+export const LazySectionsFooter = import('../../components/Sections/Footer.vue' /* webpackChunkName: "components/sections-footer" */).then(c => wrapFunctional(c.default || c))
+export const LazySectionsHeader = import('../../components/Sections/Header.vue' /* webpackChunkName: "components/sections-header" */).then(c => wrapFunctional(c.default || c))
+export const LazySectionsProject = import('../../components/Sections/Project.vue' /* webpackChunkName: "components/sections-project" */).then(c => wrapFunctional(c.default || c))
+export const LazySectionsSkill = import('../../components/Sections/Skill.vue' /* webpackChunkName: "components/sections-skill" */).then(c => wrapFunctional(c.default || c))
+export const LazyWidgetsModal = import('../../components/Widgets/Modal.vue' /* webpackChunkName: "components/widgets-modal" */).then(c => wrapFunctional(c.default || c))
+
+// nuxt/nuxt.js#8607
+export function wrapFunctional(options) {
+  if (!options || !options.functional) {
+    return options
+  }
+
+  const propKeys = Array.isArray(options.props) ? options.props : Object.keys(options.props || {})
+
+  return {
+    render(h) {
+      const attrs = {}
+      const props = {}
+
+      for (const key in this.$attrs) {
+        if (propKeys.includes(key)) {
+          props[key] = this.$attrs[key]
+        } else {
+          attrs[key] = this.$attrs[key]
+        }
+      }
+
+      return h(options, {
+        on: this.$listeners,
+        attrs,
+        props,
+        scopedSlots: this.$scopedSlots,
+      }, this.$slots.default)
+    }
+  }
+}
---
+++ .nuxt/components/plugin.js
@@ -0,0 +1,37 @@
+import Vue from 'vue'
+import { wrapFunctional } from './index'
+
+const components = {
+  ControlsMenuToggle: () => import('../../components/Controls/MenuToggle.vue' /* webpackChunkName: "components/controls-menu-toggle" */).then(c => wrapFunctional(c.default || c)),
+  ControlsThemeToggle: () => import('../../components/Controls/ThemeToggle.vue' /* webpackChunkName: "components/controls-theme-toggle" */).then(c => wrapFunctional(c.default || c)),
+  IconsDark: () => import('../../components/Icons/Dark.vue' /* webpackChunkName: "components/icons-dark" */).then(c => wrapFunctional(c.default || c)),
+  IconsEnvelope: () => import('../../components/Icons/Envelope.vue' /* webpackChunkName: "components/icons-envelope" */).then(c => wrapFunctional(c.default || c)),
+  IconsExternalLink: () => import('../../components/Icons/ExternalLink.vue' /* webpackChunkName: "components/icons-external-link" */).then(c => wrapFunctional(c.default || c)),
+  IconsFacebook: () => import('../../components/Icons/Facebook.vue' /* webpackChunkName: "components/icons-facebook" */).then(c => wrapFunctional(c.default || c)),
+  IconsGitHub: () => import('../../components/Icons/GitHub.vue' /* webpackChunkName: "components/icons-git-hub" */).then(c => wrapFunctional(c.default || c)),
+  IconsLight: () => import('../../components/Icons/Light.vue' /* webpackChunkName: "components/icons-light" */).then(c => wrapFunctional(c.default || c)),
+  IconsLinkedIn: () => import('../../components/Icons/LinkedIn.vue' /* webpackChunkName: "components/icons-linked-in" */).then(c => wrapFunctional(c.default || c)),
+  IconsLogo: () => import('../../components/Icons/Logo.vue' /* webpackChunkName: "components/icons-logo" */).then(c => wrapFunctional(c.default || c)),
+  IconsMedium: () => import('../../components/Icons/Medium.vue' /* webpackChunkName: "components/icons-medium" */).then(c => wrapFunctional(c.default || c)),
+  IconsTimes: () => import('../../components/Icons/Times.vue' /* webpackChunkName: "components/icons-times" */).then(c => wrapFunctional(c.default || c)),
+  IconsTwitter: () => import('../../components/Icons/Twitter.vue' /* webpackChunkName: "components/icons-twitter" */).then(c => wrapFunctional(c.default || c)),
+  IconsYouTube: () => import('../../components/Icons/YouTube.vue' /* webpackChunkName: "components/icons-you-tube" */).then(c => wrapFunctional(c.default || c)),
+  NavigationEmailHandle: () => import('../../components/Navigation/EmailHandle.vue' /* webpackChunkName: "components/navigation-email-handle" */).then(c => wrapFunctional(c.default || c)),
+  NavigationMobileMenu: () => import('../../components/Navigation/MobileMenu.vue' /* webpackChunkName: "components/navigation-mobile-menu" */).then(c => wrapFunctional(c.default || c)),
+  NavigationNavMenu: () => import('../../components/Navigation/NavMenu.vue' /* webpackChunkName: "components/navigation-nav-menu" */).then(c => wrapFunctional(c.default || c)),
+  NavigationNavbar: () => import('../../components/Navigation/Navbar.vue' /* webpackChunkName: "components/navigation-navbar" */).then(c => wrapFunctional(c.default || c)),
+  NavigationSocialHandles: () => import('../../components/Navigation/SocialHandles.vue' /* webpackChunkName: "components/navigation-social-handles" */).then(c => wrapFunctional(c.default || c)),
+  SectionsAbout: () => import('../../components/Sections/About.vue' /* webpackChunkName: "components/sections-about" */).then(c => wrapFunctional(c.default || c)),
+  SectionsContact: () => import('../../components/Sections/Contact.vue' /* webpackChunkName: "components/sections-contact" */).then(c => wrapFunctional(c.default || c)),
+  SectionsExperience: () => import('../../components/Sections/Experience.vue' /* webpackChunkName: "components/sections-experience" */).then(c => wrapFunctional(c.default || c)),
+  SectionsFooter: () => import('../../components/Sections/Footer.vue' /* webpackChunkName: "components/sections-footer" */).then(c => wrapFunctional(c.default || c)),
+  SectionsHeader: () => import('../../components/Sections/Header.vue' /* webpackChunkName: "components/sections-header" */).then(c => wrapFunctional(c.default || c)),
+  SectionsProject: () => import('../../components/Sections/Project.vue' /* webpackChunkName: "components/sections-project" */).then(c => wrapFunctional(c.default || c)),
+  SectionsSkill: () => import('../../components/Sections/Skill.vue' /* webpackChunkName: "components/sections-skill" */).then(c => wrapFunctional(c.default || c)),
+  WidgetsModal: () => import('../../components/Widgets/Modal.vue' /* webpackChunkName: "components/widgets-modal" */).then(c => wrapFunctional(c.default || c))
+}
+
+for (const name in components) {
+  Vue.component(name, components[name])
+  Vue.component('Lazy' + name, components[name])
+}
