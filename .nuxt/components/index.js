4ecce320ff0b97dfec915ae712ad27fdb852c47b
Freyam Mehta <frey77mehta@gmail.com>
2021-09-02 16:24:36 +0400

    consistent gitignore

--- .nuxt/components/index.js
+++ .nuxt/components/index.js
@@ -1,29 +1,29 @@
-export const ControlsMenuToggle = () => import('../../components/Controls/MenuToggle.vue' /* webpackChunkName: "components/controls-menu-toggle" */).then(c => wrapFunctional(c.default || c))
-export const ControlsThemeToggle = () => import('../../components/Controls/ThemeToggle.vue' /* webpackChunkName: "components/controls-theme-toggle" */).then(c => wrapFunctional(c.default || c))
-export const IconsDark = () => import('../../components/Icons/Dark.vue' /* webpackChunkName: "components/icons-dark" */).then(c => wrapFunctional(c.default || c))
-export const IconsEnvelope = () => import('../../components/Icons/Envelope.vue' /* webpackChunkName: "components/icons-envelope" */).then(c => wrapFunctional(c.default || c))
-export const IconsExternalLink = () => import('../../components/Icons/ExternalLink.vue' /* webpackChunkName: "components/icons-external-link" */).then(c => wrapFunctional(c.default || c))
-export const IconsFacebook = () => import('../../components/Icons/Facebook.vue' /* webpackChunkName: "components/icons-facebook" */).then(c => wrapFunctional(c.default || c))
-export const IconsGitHub = () => import('../../components/Icons/GitHub.vue' /* webpackChunkName: "components/icons-git-hub" */).then(c => wrapFunctional(c.default || c))
-export const IconsLight = () => import('../../components/Icons/Light.vue' /* webpackChunkName: "components/icons-light" */).then(c => wrapFunctional(c.default || c))
-export const IconsLinkedIn = () => import('../../components/Icons/LinkedIn.vue' /* webpackChunkName: "components/icons-linked-in" */).then(c => wrapFunctional(c.default || c))
-export const IconsLogo = () => import('../../components/Icons/Logo.vue' /* webpackChunkName: "components/icons-logo" */).then(c => wrapFunctional(c.default || c))
-export const IconsMedium = () => import('../../components/Icons/Medium.vue' /* webpackChunkName: "components/icons-medium" */).then(c => wrapFunctional(c.default || c))
-export const IconsTimes = () => import('../../components/Icons/Times.vue' /* webpackChunkName: "components/icons-times" */).then(c => wrapFunctional(c.default || c))
-export const IconsTwitter = () => import('../../components/Icons/Twitter.vue' /* webpackChunkName: "components/icons-twitter" */).then(c => wrapFunctional(c.default || c))
-export const IconsYouTube = () => import('../../components/Icons/YouTube.vue' /* webpackChunkName: "components/icons-you-tube" */).then(c => wrapFunctional(c.default || c))
-export const SectionsAbout = () => import('../../components/Sections/About.vue' /* webpackChunkName: "components/sections-about" */).then(c => wrapFunctional(c.default || c))
-export const SectionsExperience = () => import('../../components/Sections/Experience.vue' /* webpackChunkName: "components/sections-experience" */).then(c => wrapFunctional(c.default || c))
-export const SectionsFooter = () => import('../../components/Sections/Footer.vue' /* webpackChunkName: "components/sections-footer" */).then(c => wrapFunctional(c.default || c))
-export const SectionsHeader = () => import('../../components/Sections/Header.vue' /* webpackChunkName: "components/sections-header" */).then(c => wrapFunctional(c.default || c))
-export const SectionsProject = () => import('../../components/Sections/Project.vue' /* webpackChunkName: "components/sections-project" */).then(c => wrapFunctional(c.default || c))
-export const SectionsSkill = () => import('../../components/Sections/Skill.vue' /* webpackChunkName: "components/sections-skill" */).then(c => wrapFunctional(c.default || c))
-export const NavigationEmailHandle = () => import('../../components/Navigation/EmailHandle.vue' /* webpackChunkName: "components/navigation-email-handle" */).then(c => wrapFunctional(c.default || c))
-export const NavigationMobileMenu = () => import('../../components/Navigation/MobileMenu.vue' /* webpackChunkName: "components/navigation-mobile-menu" */).then(c => wrapFunctional(c.default || c))
-export const NavigationNavMenu = () => import('../../components/Navigation/NavMenu.vue' /* webpackChunkName: "components/navigation-nav-menu" */).then(c => wrapFunctional(c.default || c))
-export const NavigationNavbar = () => import('../../components/Navigation/Navbar.vue' /* webpackChunkName: "components/navigation-navbar" */).then(c => wrapFunctional(c.default || c))
-export const NavigationSocialHandles = () => import('../../components/Navigation/SocialHandles.vue' /* webpackChunkName: "components/navigation-social-handles" */).then(c => wrapFunctional(c.default || c))
-export const WidgetsModal = () => import('../../components/Widgets/Modal.vue' /* webpackChunkName: "components/widgets-modal" */).then(c => wrapFunctional(c.default || c))
+export { default as ControlsMenuToggle } from '../../components/Controls/MenuToggle.vue'
+export { default as ControlsThemeToggle } from '../../components/Controls/ThemeToggle.vue'
+export { default as IconsDark } from '../../components/Icons/Dark.vue'
+export { default as IconsEnvelope } from '../../components/Icons/Envelope.vue'
+export { default as IconsExternalLink } from '../../components/Icons/ExternalLink.vue'
+export { default as IconsFacebook } from '../../components/Icons/Facebook.vue'
+export { default as IconsGitHub } from '../../components/Icons/GitHub.vue'
+export { default as IconsLight } from '../../components/Icons/Light.vue'
+export { default as IconsLinkedIn } from '../../components/Icons/LinkedIn.vue'
+export { default as IconsLogo } from '../../components/Icons/Logo.vue'
+export { default as IconsMedium } from '../../components/Icons/Medium.vue'
+export { default as IconsTimes } from '../../components/Icons/Times.vue'
+export { default as IconsTwitter } from '../../components/Icons/Twitter.vue'
+export { default as IconsYouTube } from '../../components/Icons/YouTube.vue'
+export { default as NavigationEmailHandle } from '../../components/Navigation/EmailHandle.vue'
+export { default as NavigationMobileMenu } from '../../components/Navigation/MobileMenu.vue'
+export { default as NavigationNavMenu } from '../../components/Navigation/NavMenu.vue'
+export { default as NavigationNavbar } from '../../components/Navigation/Navbar.vue'
+export { default as NavigationSocialHandles } from '../../components/Navigation/SocialHandles.vue'
+export { default as SectionsAbout } from '../../components/Sections/About.vue'
+export { default as SectionsExperience } from '../../components/Sections/Experience.vue'
+export { default as SectionsFooter } from '../../components/Sections/Footer.vue'
+export { default as SectionsHeader } from '../../components/Sections/Header.vue'
+export { default as SectionsProject } from '../../components/Sections/Project.vue'
+export { default as SectionsSkill } from '../../components/Sections/Skill.vue'
+export { default as WidgetsModal } from '../../components/Widgets/Modal.vue'
 
 // nuxt/nuxt.js#8607
 function wrapFunctional(options) {
