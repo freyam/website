c012fc2b72c261333ed81a68e0466676039ebdde
Freyam Mehta <frey77mehta@gmail.com>
2021-11-30 08:18:57 +0400

    Added ISB Experience

---
+++ dist/_nuxt/static/1638245855/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1638245855",layout:"default",error:a,state:{menu:b,modal:{active:b,content:a},theme:a},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"03314598"}}}}(null,false,"\u002F"));
