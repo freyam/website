af034920df065d95b0344c962d1e676a11a98805
Freyam Mehta <frey77mehta@gmail.com>
2022-08-30 12:25:19 +0530

    Upgraded Yarn

--- dist/_nuxt/static/1656105269/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1656105269",layout:"default",error:a,state:{menu:b,modal:{active:b,content:a},theme:a},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"db5010bc"}}}}(null,false,"\u002F"));
